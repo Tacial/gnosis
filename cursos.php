949890b69c553fac2155a4a149f6ed09937564e6
Tacial-Services <tacial@hotmail.com>
2019-04-06 15:36:17 -0300

    upfiles0001

---
+++ cursos.php
@@ -0,0 +1,44 @@
+<html>
+ <head>
+  <link rel="stylesheet" type="text/css" href="style2.css">
+  <meta http-equiv="content-type" content="text/html" />
+  <meta charset="UTF-8" />
+  <meta name="author" content="tc" />
+  <meta name="Description" content="Programaçao / webdesign / homeoffice">
+  <meta name="Keywords" content="programacao, website, homeoffice">
+  <title> . </title>
+ </head>
+ <body>
+  <div id="global">
+   <div id="cabecalho">
+    <?php
+     include "cabeca.inc"; 
+     include "menu.inc";
+    ?>
+   </div>
+   <p>se você quer potencializar o aprendizado de qualquer coisa,<br />
+      você deve estudar sobre esse determinado assunto logo quando acordar,<br />
+      e antes de dormir...de preferência com música!!!</p>
+   <hr>
+   <br>
+   <section>
+
+     <a href="/FraServicos/daaa.php" target="_blank" >Projeto Do Alicerce Ao Acabamento</a></br>
+     <a href="/fab/projeto.html" target="_blank" >Projeto Dantas Serviços</a><br />
+     <a href="/libr/Livraria.html" target="_blank" >Projeto Sinal Verde - Livraria</a><br/>
+     <a href="Allnet/projeto.html" target="_blank" >Projeto AllNet</a></br>
+     <a href="gih/ciadodoce.php" target="_blank" >Projeto Cia Do Doce</a></br>
+     <a href="Joao/joao.html" target="_blank" >Projeto A & R Serviços</a></br>
+     <a href="/Faculdade/index.html" target="_blank" >Projeto Faculdade</a></br>
+     <a href="/EscTec/index.html" target="_blank" >Projeto EscTec</a><br/>     
+     <a href="/Pto-a-Pto/index.php" target="_blank" >Ponto a Ponto</a></br>
+   </section>
+   <aside>
+    <img src="meta.jpg" />
+   </aside>
+   <footer id="rodape" />
+    <a href="index.php">Voltar</a>
+    <?php include "rodape.inc"; ?>
+   </footer>
+  </body>
+</html>
---
+++ divulga.php
@@ -0,0 +1,52 @@
+
+<html>
+ <head>
+  <link rel="stylesheet" type="text/css" href="style2.css">
+  <meta http-equiv="content-type" content="text/html" />
+  <meta charset="UTF-8" />
+  <meta name="author" content="tc" />
+  <meta name="Description" content="Procura um serviço de programaçao - webdesign homeoffice? A TCiocca oferece o melhor... ">
+  <meta name="Keywords" content="programacao, website, homeoffice">
+    <title> . </title>
+
+ </head>
+ <body>
+  <div id="global">
+   <!-- div cabecalho-->
+   <?php
+     include "cabeca.inc"; 
+     include "menu.inc";
+   ?>
+
+   <form align="center">
+    <hr>
+     <table cellpadding="2" cellspacing="5">
+      <tr><td><img src="seja-o-exemplo.jpg" width="330px" height="380px" ></td>
+       <td><pre>                         </pre></td>
+       <td>
+         <h3>O Bom e Velho Rock'n'Roll...</h3>
+         <a href="http://kissfm.com.br/"> Kiss FM - 102.1 (SP) </a><br />
+         <!-- a href="http://www.rocknation.com.br/"> RockNation </a --><br />   
+
+         <h3>Serviços</h3>
+         <a href="http://www.facebook.com/DoAlicerceAoAcabamento" target="_blank">Do Alicerce Ao Acabamento</a> / Reformas<br />
+         <a href="http://www.facebook.com/gihciadodoce" target="_blank" >Cia do Doce</a><br />
+         
+         <h3>Cursos OnLine</h3>
+         <a href="https://www.duolingo.com">Duolingo</a> - Inglês, Espanhol, Francês e Alemão<br/>        
+         <a href="http://www.ev.org.br/Paginas/Home.aspx">Fundação Bradesco</a> - diversos<br/> 
+         <a href="https://www.microsoftvirtualacademy.com">Microsoft Virtual Academy</a> - diversos em programação<br/>        
+         <a href="https://www.cursoemvideo.com">Curso em Video</a> - diversos - Gustavo Guanabara<br/>        
+         <a href="https://www.brasilmaisti.com.br">Brasil Mais TI</a> - diversos<br/>
+         <a href="https://www.udemy.com">Udemy</a> - diversos<br/>
+
+       </td>
+      </tr>
+     </table>
+
+    <br />
+    <hr>    
+    <a href="index.php">Voltar</a>
+    <?php include "rodape.inc"; ?>
+  </body>
+</html>
